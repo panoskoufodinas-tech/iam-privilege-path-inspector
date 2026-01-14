0e22c3b2d46c81b8105c26cf9c1e67b8352df3bb
Panagiotis Ntinas <panos.koufodinas@gmail.com>
2026-01-14 11:05:58 +0200

    Update ippi.py

--- ippi.py
+++ ippi.py
@@ -1,4 +1,16 @@
-import subprocess, json, os
+# Copyright 2026 panoskoufodinas-tech
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.import subprocess, json, os
 def run():
     print("="*40 + "\n IPPI v2.0\n" + "="*40)
     proj = subprocess.getoutput("gcloud config get-value project 2>/dev/null").split()[-1]
